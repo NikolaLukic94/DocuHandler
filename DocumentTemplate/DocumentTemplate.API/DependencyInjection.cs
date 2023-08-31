fe72d1d586aa214b64ed465fdf2f66241679e2be
NikolaLukic94 <nikola94lukic@hotmail.com>
2023-08-31 22:55:13 +0200

    Move microservices & gateway initial setup

--- DocumentTemplate/DocumentTemplate.API/DependencyInjection.cs
+++ DocumentTemplate/DocumentTemplate.API/DependencyInjection.cs
@@ -1,6 +1,7 @@
 
 using DocumentTempate.API.Common.Errors;
 using DocumentTempate.API.Common.Mapping;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace DocumentTemplate.API;
@@ -12,6 +13,20 @@ public static class DependencyInjection
         services.AddControllers();
         services.AddSingleton<ProblemDetailsFactory, DocumentTemplateProblemDetailsFactory>();
         services.AddMappings();
+
+        services.Configure<IdentityOptions>(options =>
+        {
+            // Account lockout settings
+            options.Lockout.MaxFailedAccessAttempts = 5; // Number of failed attempts before lockout
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30); // Lockout duration
+
+            // Password policy settings
+            options.Password.RequiredLength = 8; // Minimum password length
+            options.Password.RequireDigit = true; // Require at least one digit
+            options.Password.RequireNonAlphanumeric = true; // Require at least one non-alphanumeric character
+            options.Password.RequireUppercase = true; // Require at least one uppercase letter
+            options.Password.RequireLowercase = true; // Require at least one lowercase letter
+        });
 
         return services;
     }
