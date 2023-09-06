ca2fb87d588096a11d9cd1ff12e65f05543f32ae
NikolaLukic94 <nikola94lukic@hotmail.com>
2023-09-06 18:27:53 +0200

    Ocelot setup finished

--- DocumentTemplate/ApiGateway/ApiGateway/Program.cs
+++ DocumentTemplate/ApiGateway/ApiGateway/Program.cs
@@ -1,17 +1,41 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
-builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+// Add configuration
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 
+// Add services
 builder.Services.AddOcelot(builder.Configuration);
 
+// Configure services
 var app = builder.Build();
 
-await app.UseOcelot();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+// Use Ocelot middleware
+app.UseOcelot();
 
 app.Run();
+
+
+
+
